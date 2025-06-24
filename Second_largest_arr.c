dd04449b855836413909670bbc88bc87603fca47
kabilan <108561329+kabilan-karthi@users.noreply.github.com>
2025-06-24 09:11:41 +0530

    Create Second_largest_arr.c

---
+++ Second_largest_arr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <limits.h>
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int first = INT_MIN;
+    int second = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    if (second == INT_MIN) {
+        printf("There is no second largest element.\n");
+    } else {
+        printf("The second largest element is %d\n", second);
+    }
+
+    return 0;
+}
